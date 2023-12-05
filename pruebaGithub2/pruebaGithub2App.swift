d7b0e1c5b527d3fc8a6f13b8395fc35ef7335ae7
AlekseyOberstein <420035969@pcpuma.acatlan.unam.mx>
2023-12-05 10:25:07 -0600

    Initial Commit

---
+++ pruebaGithub2/pruebaGithub2App.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaGithub2App.swift
+//  pruebaGithub2
+//
+//  Created by Alexis Palomares Olegario on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebaGithub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
